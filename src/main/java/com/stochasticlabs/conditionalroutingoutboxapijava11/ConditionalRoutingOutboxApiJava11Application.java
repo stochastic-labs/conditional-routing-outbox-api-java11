2b21fc9aefa460738fd30d62667331a3f7873d96
Lucas Dumer <lucasdumer@gmail.com>
2026-05-30 18:07:09 -0300

    first commit

---
+++ src/main/java/com/stochasticlabs/conditionalroutingoutboxapijava11/ConditionalRoutingOutboxApiJava11Application.java
@@ -0,0 +1,11 @@
+package com.stochasticlabs.conditionalroutingoutboxapijava11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConditionalRoutingOutboxApiJava11Application {
+    public static void main(String[] args) {
+        SpringApplication.run(ConditionalRoutingOutboxApiJava11Application.class, args);
+    }
+}
---
+++ src/test/java/com/stochasticlabs/conditionalroutingoutboxapijava11/ConditionalRoutingOutboxApiJava11ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.stochasticlabs.conditionalroutingoutboxapijava11;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ConditionalRoutingOutboxApiJava11ApplicationTests {
+
+    @Test
+    @DisplayName("Should start Spring Application!")
+    void main_springApplication_start() {
+        ConditionalRoutingOutboxApiJava11Application.main(new String[] {"--server.port=0"});
+    }
+}
