65cfe96097d1f372249edbd7db1eb9db6f2f0f5e
Lucas Dumer <lucasdumer@gmail.com>
2026-05-31 17:50:06 -0300

    add inputDTO

---
+++ src/main/java/com/stochasticlabs/conditionalroutingoutboxapijava11/dto/InputDTO.java
@@ -0,0 +1,12 @@
+package com.stochasticlabs.conditionalroutingoutboxapijava11.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InputDTO {
+    private int integer;
+}
