c09aa400676e2ee13165718cd25a8491d9682a52
Lucas Dumer <lucasdumer@gmail.com>
2026-05-31 17:51:32 -0300

    add service folder

---
+++ src/main/java/com/stochasticlabs/conditionalroutingoutboxapijava11/service/RoutingService.java
@@ -0,0 +1,24 @@
+package com.stochasticlabs.conditionalroutingoutboxapijava11.service;
+
+import com.stochasticlabs.conditionalroutingoutboxapijava11.dto.InputDTO;
+import com.stochasticlabs.conditionalroutingoutboxapijava11.strategy.RoutingStrategy;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class RoutingService {
+
+    private final List<RoutingStrategy> strategies;
+
+    public RoutingService(List<RoutingStrategy> strategies) {
+        this.strategies = strategies;
+    }
+
+    public void process(Integer number) {
+        List<RoutingStrategy> activeStrategies = strategies.stream()
+                .filter(strategy -> strategy.isEligible(number))
+                .collect(Collectors.toList());
+    }
+}
