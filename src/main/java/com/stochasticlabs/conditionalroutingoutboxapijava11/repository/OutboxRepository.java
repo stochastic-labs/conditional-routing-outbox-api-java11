ff7e397acb145d7288d8de2eeef22108aab7736c
Lucas Dumer <lucasdumer@gmail.com>
2026-06-04 11:36:11 -0300

    add outbox repository

---
+++ src/main/java/com/stochasticlabs/conditionalroutingoutboxapijava11/repository/OutboxRepository.java
@@ -0,0 +1,14 @@
+package com.stochasticlabs.conditionalroutingoutboxapijava11.repository;
+
+import com.stochasticlabs.conditionalroutingoutboxapijava11.domain.OutboxStatus;
+import com.stochasticlabs.conditionalroutingoutboxapijava11.entity.Outbox;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OutboxRepository extends JpaRepository<Outbox, Long> {
+    Page<Outbox> findByStatus(OutboxStatus status, Pageable pageable);
+    List<Outbox> findTop10ByStatusOrderByIdAsc(OutboxStatus status);
+}
