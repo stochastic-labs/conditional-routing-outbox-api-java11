bbae97182b61eff7680ff5dfcd75ac366530f3e2
Lucas Dumer <lucasdumer@gmail.com>
2026-06-04 11:30:35 -0300

    feat add enableScheduling

--- src/main/java/com/stochasticlabs/conditionalroutingoutboxapijava11/ConditionalRoutingOutboxApiJava11Application.java
+++ src/main/java/com/stochasticlabs/conditionalroutingoutboxapijava11/ConditionalRoutingOutboxApiJava11Application.java
@@ -2,7 +2,9 @@ package com.stochasticlabs.conditionalroutingoutboxapijava11;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class ConditionalRoutingOutboxApiJava11Application {
     public static void main(String[] args) {
