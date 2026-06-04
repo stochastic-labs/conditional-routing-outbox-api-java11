ff1048267e57e56bc14660429f0b44af691b379e
Lucas Dumer <lucasdumer@gmail.com>
2026-06-03 23:29:01 -0300

    add Input entity

---
+++ src/main/java/com/stochasticlabs/conditionalroutingoutboxapijava11/entity/Input.java
@@ -0,0 +1,12 @@
+package com.stochasticlabs.conditionalroutingoutboxapijava11.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Input {
+    private int integer;
+}
