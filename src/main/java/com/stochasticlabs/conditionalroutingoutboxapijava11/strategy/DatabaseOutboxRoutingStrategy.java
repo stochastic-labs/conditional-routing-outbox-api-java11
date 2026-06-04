3e982cee1249c07e3665472c632d51c95842bd1c
Lucas Dumer <lucasdumer@gmail.com>
2026-06-04 11:32:37 -0300

    feat database outbox

--- src/main/java/com/stochasticlabs/conditionalroutingoutboxapijava11/strategy/DatabaseOutboxRoutingStrategy.java
+++ src/main/java/com/stochasticlabs/conditionalroutingoutboxapijava11/strategy/DatabaseOutboxRoutingStrategy.java
@@ -1,13 +1,25 @@
 package com.stochasticlabs.conditionalroutingoutboxapijava11.strategy;
 
-import com.stochasticlabs.conditionalroutingoutboxapijava11.dto.InputDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.stochasticlabs.conditionalroutingoutboxapijava11.domain.OutboxStatus;
 import com.stochasticlabs.conditionalroutingoutboxapijava11.entity.Input;
+import com.stochasticlabs.conditionalroutingoutboxapijava11.entity.Outbox;
+import com.stochasticlabs.conditionalroutingoutboxapijava11.repository.OutboxRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Slf4j
-@Component
+@Service
+@RequiredArgsConstructor
 public class DatabaseOutboxRoutingStrategy implements RoutingStrategy {
+
+    private final OutboxRepository outboxRepository;
+
+    private final ObjectMapper objectMapper;
 
     @Override
     public boolean validate(Input input) {
@@ -15,7 +27,14 @@ public class DatabaseOutboxRoutingStrategy implements RoutingStrategy {
     }
 
     @Override
-    public void execute(Input input) {
+    public void execute(Input input) throws JsonProcessingException {
         log.info("Send [" + input.getInteger() + "] to DB.");
+        Outbox outbox = Outbox.builder()
+                .topic("stochastic-input")
+                .payload(objectMapper.writeValueAsString(input))
+                .status(OutboxStatus.PENDING)
+                .createdAt(LocalDateTime.now())
+                .build();
+        outboxRepository.save(outbox);
     }
 }
