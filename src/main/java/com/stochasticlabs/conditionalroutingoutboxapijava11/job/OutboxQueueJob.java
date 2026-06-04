1ab39930cac98accbf55d92b8e67580240f6d007
Lucas Dumer <lucasdumer@gmail.com>
2026-06-04 12:28:17 -0300

    feat outbox job use config

--- src/main/java/com/stochasticlabs/conditionalroutingoutboxapijava11/job/OutboxQueueJob.java
+++ src/main/java/com/stochasticlabs/conditionalroutingoutboxapijava11/job/OutboxQueueJob.java
@@ -2,16 +2,18 @@ package com.stochasticlabs.conditionalroutingoutboxapijava11.job;
 
 import com.stochasticlabs.conditionalroutingoutboxapijava11.service.OutboxService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@ConditionalOnProperty(value = "app.outbox.enabled", havingValue = "true", matchIfMissing = false)
 public class OutboxQueueJob {
 
     private final OutboxService outboxService;
 
-    @Scheduled(fixedDelay = 5000)
+    @Scheduled(fixedDelayString = "${app.outbox.interval:5000}")
     public void execute() {
         outboxService.execute();
     }
