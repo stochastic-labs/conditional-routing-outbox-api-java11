46de61cba9843ccbc56a4b12928e18b55a1fcd03
Lucas Dumer <lucasdumer@gmail.com>
2026-06-05 16:47:18 -0300

    feat http strategy code

--- src/main/java/com/stochasticlabs/conditionalroutingoutboxapijava11/strategy/HttpRoutingStrategy.java
+++ src/main/java/com/stochasticlabs/conditionalroutingoutboxapijava11/strategy/HttpRoutingStrategy.java
@@ -1,15 +1,31 @@
 package com.stochasticlabs.conditionalroutingoutboxapijava11.strategy;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.stochasticlabs.conditionalroutingoutboxapijava11.entity.Input;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class HttpRoutingStrategy implements RoutingStrategy {
+
+    private final ObjectMapper objectMapper;
+
+    private final HttpClient httpClient;
+
+    @Value("${app.services.java17-api-url}")
+    private String url;
 
     @Override
     public boolean validate(Input input) {
@@ -17,7 +33,36 @@ public class HttpRoutingStrategy implements RoutingStrategy {
     }
 
     @Override
+    @CircuitBreaker(name = "java17HttpStrategy", fallbackMethod = "fallbackHttpCall")
     public void execute(Input input) throws JsonProcessingException {
         log.info("Send [" + input.getInteger() + "] to API.");
+
+        String jsonPayload = objectMapper.writeValueAsString(input);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url+"/api/v1/idempotent/input"))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(jsonPayload))
+                .timeout(Duration.ofSeconds(3))
+                .build();
+
+        try {
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 201 || response.statusCode() == 200) {
+                log.info("HttpRoutingStrategy Success: Payload. Status: {}", response.statusCode());
+            } else {
+                log.warn("HttpRoutingStrategy Fail HTTP: API {}. error Circuit Breaker.", response.statusCode());
+                throw new RuntimeException("Fail");
+            }
+        } catch (Exception e) {
+            log.error("HttpRoutingStrategy Error: API Java 17.", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void fallbackHttpCall(Input input, Throwable t) {
+        log.error("🚨 [CIRCUIT BREAKER active / Fail Request] Payload [{}]. Error: {}",
+                input.getInteger(), t.getMessage());
     }
 }
