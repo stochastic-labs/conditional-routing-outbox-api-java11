6ff6d8336216212b57f030e4606c6467e04c8c12
Lucas Dumer <lucasdumer@gmail.com>
2026-06-05 22:00:29 -0300

    feat use env enable http api

--- src/main/java/com/stochasticlabs/conditionalroutingoutboxapijava11/ConditionalRoutingOutboxApiJava11Application.java
+++ src/main/java/com/stochasticlabs/conditionalroutingoutboxapijava11/ConditionalRoutingOutboxApiJava11Application.java
@@ -1,6 +1,7 @@
 package com.stochasticlabs.conditionalroutingoutboxapijava11;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -8,6 +9,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class ConditionalRoutingOutboxApiJava11Application {
     public static void main(String[] args) {
-        SpringApplication.run(ConditionalRoutingOutboxApiJava11Application.class, args);
+        SpringApplication app = new SpringApplication(ConditionalRoutingOutboxApiJava11Application.class);
+
+        String apiEnabled = System.getenv("APP_API_HTTP_ENABLED");
+
+        if ("false".equalsIgnoreCase(apiEnabled)) {
+            app.setWebApplicationType(WebApplicationType.NONE);
+        }
+
+        app.run(args);
     }
 }
